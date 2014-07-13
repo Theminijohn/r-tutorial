98b8f6b930614e5551a1bb895200b87ace76357a
The Mini John <admin@phoenix-prime.gr>
2014-07-13 15:02:32 +0300

    Working with Variables

---
+++ BasicMath.r
@@ -0,0 +1,36 @@
+1+1
+1+2+3
+7-5
+8+9-6
+3*4
+4*3*2
+4/2
+4/3
+
+# PEMDAS ->
+4* 6+5
+4* (6+5)
+
+# Working with Variables ->
+x <- 2
+x
+
+z <- 3
+z
+
+# Serial Assignment ->
+a <- b <- 7
+a
+b
+
+# Assign Variable ->
+assign("j", 4)
+j
+
+# Remove Variable ->
+rm(j)
+j
+
+# R is case sensitive
+
+
